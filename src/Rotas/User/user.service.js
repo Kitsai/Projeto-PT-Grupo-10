61eef18337c10401c6e5ce27d0ede5b1e7238caf
Kitsai <lucas.rocha.3801@gmail.com>
2023-11-17 19:48:25 -0300

    feat:base auth baseado nas aulas

---
+++ src/Rotas/User/user.service.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class UserService {
+    async create(username, password, profile_picture, gender, email, jobTitle) {
+        return await prisma.user.create({
+            data: {
+                username,
+                password,
+                profile_picture,
+                gender,
+                email,
+                jobTitle //?
+            }
+        }).catch(e => {
+            if(e.code === 'P2002') {
+                throw new Error('Usuário já existe')
+            }
+            throw e
+        })
+    }
+
+    async findbyEmail(email) {
+        return await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+    }
+}
+
+export default UserService
---
+++ src/auth/auth.service.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import UserService from '../Rotas/User/user.service.js';
+
+const userService = new UserService();
+
+class AuthService {
+    async signIn (email, senha) {
+        const user = await userService.findbyEmail(email);
+
+        if(!user) {
+            throw new Error('Usuário não encontrado');
+        }
+
+        if(!await bcrypt.compare(senha, user.password)) {
+            throw new Error('Senha incorreta');
+        }
+
+        const token = jwt.sign({ id: user.id }, "UltraSecretoUau")
+
+        return token;
+    }
+
+    async signUp (username, password, profile_picture, gender, email, jobTitle) {
+        const salt = await bcrypt.genSalt();
+
+        password = await bcrypt.hash(password, salt);
+
+        const newUser = await userService.create(username,password,profile_picture,gender,email,jobTitle);
+    }
+}
+
+export default AuthService;
