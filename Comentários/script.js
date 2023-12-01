4817c54e2cbbeef1f7fe87ae4b88a2b558347d97
Ithalo Junio Medeiros de O. Nóbrega <ithalomedei76@gmail.com>
2023-12-01 00:23:21 -0300

    Començando as telas de comentários e suas especificações

--- Comentários/script.js
+++ Comentários/script.js
@@ -1,3 +1,17 @@
-import {mockedPosts} from "../src/Feed_posts";
+import renderHeader_Comen from '../src/Header_Comen.js'
+import renderComen from './Render_Comen.js'
+
+const modal = document.getElementById("modalPost");
+
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//event prevent default
+
+renderHeader_Comen("comen")
+renderComen()
 
 
---
+++ src/Header_Comen.js
@@ -0,0 +1,57 @@
+import renderComen from "../Comentários/Render_Comen.js";
+
+export default function renderHeader (page) {
+
+    const token = sessionStorage.getItem('token');
+    const username = sessionStorage.getItem('username');
+    const profile_picture = sessionStorage.getItem('profile_picture');
+
+
+    const header = document.body.querySelector("header")
+
+    if(header.querySelector(".header_content") != null) {
+        header.removeChild(header.querySelector(".header_content"))
+    }
+
+    const header_content = document.createElement("div")
+    header_content.id = (token)? "header_logado" : "header_deslogado"
+    header_content.classList.add("header_content")
+
+    if(token) {
+        header_content.innerHTML = '<a href="" class="header_profile"><img src="'
+        + profile_picture + '" alt="profile picture"> '
+        + username + '</a>'
+
+        const logout = document.createElement("button")
+        logout.id = 'exit_button'
+        logout.type = 'button'
+        logout.innerHTML = '<img src="../assets/exit-14.svg" alt="exit">'
+
+        logout.addEventListener("click", exit_button_clicked,false)
+        logout.myParam = page
+
+        header_content.appendChild(logout)
+    } else {
+        header_content.innerHTML = '<a id="header_cadastro" href="../tela de cadastro/página de cadastro.html">Crie uma conta</a>'
+        + '<a id="header_entrar" href="../home/página de login.html">ENTRAR</a>'
+    }
+
+    header.appendChild(header_content)
+}
+
+const exit_button_clicked = (evt) => {
+    const page = evt.currentTarget.myParam
+    
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('username');
+    sessionStorage.removeItem('profile_picture');
+
+    switch(page) {
+        case "comen":
+            renderComen(null)
+            break
+        default:
+            break
+    }
+    renderHeader(page)
+}
