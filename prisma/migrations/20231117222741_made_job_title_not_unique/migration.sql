3ca25b4008fd44d9eed8778b0c1fbfb704299e97
Rafael Ghiorzi <rafael.ghiorzi@gmail.com>
2023-12-05 22:31:24 -0300

    Delete prisma directory

--- prisma/migrations/20231117222741_made_job_title_not_unique/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "User_jobTitle_key";
--- prisma/migrations/20231119033115_/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "Comment_authorId_key";
-
--- DropIndex
-DROP INDEX "Comment_postId_key";
-
--- DropIndex
-DROP INDEX "Post_authorId_key";
